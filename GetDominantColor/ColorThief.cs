e6070c718248d9f992e9f39d6d3d811c3e175495
SamCoder1025 <kundankumar8377@gmail.com>
2020-03-06 12:01:19 +0530

    Add exsiting files

---
+++ GetDominantColor/ColorThief.cs
@@ -0,0 +1,99 @@
+﻿using ColorThiefDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+
+namespace GetDominantColor
+{
+    public partial class CustomColorThief
+    {
+        private const int DefaultQuality = 10;
+        private const bool DefaultIgnoreWhite = true;
+        private const int DefaultColorCount = 8;
+
+        /// <summary>
+        ///     Use the median cut algorithm to cluster similar colors and return the base color from the largest cluster.
+        /// </summary>
+        /// <param name="sourceImage">The source image.</param>
+        /// <param name="quality">
+        ///     1 is the highest quality settings. 10 is the default. There is
+        ///     a trade-off between quality and speed. The bigger the number,
+        ///     the faster a color will be returned but the greater the
+        ///     likelihood that it will not be the visually most dominant color.
+        /// </param>
+        /// <param name="ignoreWhite">if set to <c>true</c> [ignore white].</param>
+        /// <returns></returns>
+        public async Task<QuantizedColor> GetColor(BitmapDecoder sourceImage, int quality = DefaultQuality, bool ignoreWhite = DefaultIgnoreWhite)
+        {
+            var palette = await GetPalette(sourceImage, 3, quality, ignoreWhite);
+
+            var dominantColor = new QuantizedColor(new Color
+            {
+                A = Convert.ToByte(palette.Average(a => a.Color.A)),
+                R = Convert.ToByte(palette.Average(a => a.Color.R)),
+                G = Convert.ToByte(palette.Average(a => a.Color.G)),
+                B = Convert.ToByte(palette.Average(a => a.Color.B))
+            }, Convert.ToInt32(palette.Average(a => a.Population)));
+
+            return dominantColor;
+        }
+
+        /// <summary>
+        ///     Use the median cut algorithm to cluster similar colors.
+        /// </summary>
+        /// <param name="sourceImage">The source image.</param>
+        /// <param name="colorCount">The color count.</param>
+        /// <param name="quality">
+        ///     1 is the highest quality settings. 10 is the default. There is
+        ///     a trade-off between quality and speed. The bigger the number,
+        ///     the faster a color will be returned but the greater the
+        ///     likelihood that it will not be the visually most dominant color.
+        /// </param>
+        /// <param name="ignoreWhite">if set to <c>true</c> [ignore white].</param>
+        /// <returns></returns>
+        /// <code>true</code>
+        public async Task<List<QuantizedColor>> GetPalette(BitmapDecoder sourceImage, int colorCount = DefaultColorCount, int quality = DefaultQuality, bool ignoreWhite = DefaultIgnoreWhite)
+        {
+            var pixelArray = await GetPixelsFast(sourceImage, quality, ignoreWhite);
+            var cmap = GetColorMap(pixelArray, colorCount);
+            if (cmap != null)
+            {
+                //var colors = cmap.GeneratePalette();
+                //return colors;
+            }
+            return new List<QuantizedColor>();
+        }
+
+        private object GetColorMap(byte[][] pixelArray, int colorCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<byte[]> GetIntFromPixel(BitmapDecoder decoder)
+        {
+            var pixelsData = await decoder.GetPixelDataAsync();
+            var pixels = pixelsData.DetachPixelData();
+            return pixels;
+        }
+
+        private async Task<byte[][]> GetPixelsFast(BitmapDecoder sourceImage, int quality, bool ignoreWhite)
+        {
+            if (quality < 1)
+            {
+                quality = DefaultQuality;
+            }
+
+            var pixels = await GetIntFromPixel(sourceImage);
+            var pixelCount = sourceImage.PixelWidth * sourceImage.PixelHeight;
+
+            return ConvertPixels(pixels, Convert.ToInt32(pixelCount), quality, ignoreWhite);
+        }
+
+        private byte[][] ConvertPixels(byte[] pixels, int v, int quality, bool ignoreWhite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GetDominantColor/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using ColorThiefDotNet;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace GetDominantColor
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private Brush _bgColor;
+        private string _fileName;
+
+        public Brush BgColor
+        {
+            get => this._bgColor;
+            set
+            {
+                if (value != this._bgColor)
+                {
+                    this._bgColor = value;
+                    NotifyPropertyChanged("BgColor");
+                }
+            }
+        }
+
+        public string FileName
+        {
+            get => this._fileName;
+            set
+            {
+                if (value != this._fileName)
+                {
+                    this._fileName = value;
+                    NotifyPropertyChanged("FileName");
+                }
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // PropertyChanged logic
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Get storage file async
+        private async Task<StorageFile> GetFileAsync()
+        {
+            StorageFile file = null;
+            try
+            {
+                var picker = new FileOpenPicker();
+                picker.ViewMode = PickerViewMode.Thumbnail;
+                picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                picker.FileTypeFilter.Add(".jpg");
+                picker.FileTypeFilter.Add(".jpeg");
+                picker.FileTypeFilter.Add(".png");
+
+                file = await picker.PickSingleFileAsync();
+            }
+            catch { }
+            return file;
+        }
+
+        private async void SelectImage(object sender, RoutedEventArgs e)
+        {
+            StorageFile file = await GetFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                fileName.Text = "Picked photo: " + file.Name;
+
+                IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
+                // Create the decoder from the stream
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                var colorThief = new ColorThief();
+                var dominantColor = await colorThief.GetColor(decoder);
+                var color = dominantColor.Color;
+
+                Rect.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(color.A,
+                                                                                color.R,
+                                                                                color.G,
+                                                                                color.B));
+            }
+        }
+    }
+}
